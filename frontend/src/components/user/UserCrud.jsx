17d311fbde7749889d51aefa391ee3f0c6e3d551
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-13 15:11:00 -0300

    feat: implement user form with state management and axios integration

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -39,10 +39,69 @@ export default class UserCrud extends Component {
         return list
     }
 
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState ({ user })
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite seu nome..." />
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-mds-6">
+                        <di className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite seu email..." />
+                        </di>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-contend-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    renderTable() {
+
+    }
+
+    renderRow() {
+
+    }
+
     render() {
      return (
         <Main {...headerProps}>
-            Cadastro de Usuário
+            {this.renderForm()}
         </Main>
      )   
     }
